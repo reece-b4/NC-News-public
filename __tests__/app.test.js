83fdd3c5c6c046fe8e9b2169414541474293320a
reece-b4 <reecebalfour@gmail.com>
2022-02-15 14:55:41 +0000

    complete ticket 7, refactor controller and model folders/files

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const db = require("../db/connection.js");
 
-const testData = require("../db/data/test-data/");
+const testData = require("../db/data/test-data");
 const app = require("../app.js");
 const seed = require("../db/seeds/seed.js");
 
@@ -68,7 +68,7 @@ describe("/api/articles/:article_id", () => {
           expect(res.body.msg).toBe("bad request");
         });
     });
-    test("404 - given correct id with no data, returns message: not found", () => {
+    test("404 - given non existent id with no data, returns message: not found", () => {
       return request(app)
         .get("/api/articles/999999")
         .expect(404)
@@ -77,16 +77,62 @@ describe("/api/articles/:article_id", () => {
         });
     });
   });
-  describe('PATCH', ()=> {
-      test('accepts body in form of { inc_votes: newVote } with newVote dictating the inc/decrement of votes. Responds with updated article', ()=>{
 
-      })
-  })
-});
-
-describe('', ()=> {
-
+  //ticket 7
+  describe("PATCH", () => {
+    test("200 - accepts body in form of { inc_votes: newVote } with newVote dictating the inc/decrement of votes. Responds with updated article", () => {
+      const votes = { inc_votes: -10 };
+      return request(app)
+        .patch("/api/articles/1")
+        .send(votes)
+        .expect(200)
+        .then((res) => {
+          expect(res.body.article).toEqual({
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: expect.any(String),
+            votes: 90,
+          });
+        });
+    });
+    //already covered by get request error?
+    test("400 - given invalid id data type, returns message: bad request", () => {
+      const votes = { inc_votes: -10 };
+      return request(app)
+        .patch("/api/articles/not-an-id")
+        .send(votes)
+        .expect(400)
+        .then((res) => {
+          expect(res.body.msg).toBe("bad request");
+        });
+    });
+    test("404 - given correct id with no data, returns message: not found", () => {
+      const votes = { inc_votes: -10 };
+      return request(app)
+        .patch("/api/articles/999999")
+        .send(votes)
+        .expect(404)
+        .then((res) => {
+          expect(res.body.msg).toBe("not found");
+        });
+    });
+    //if no code change needed outside of test, is the error test redundant?
+    test(" 400- given incorrect votes data type returns message: bad request", () => {
+      const votes = { inc_votes: 'not-a-valid-vote-count' };
+      return request(app)
+        .patch("/api/articles/999999")
+        .send(votes)
+        .expect(400)
+        .then((res) => {
+          expect(res.body.msg).toBe("bad request");
+        });
+    });
+  });
 })
+
 
 //promise.all in controller to prevent conflicts of empty array being 200 and 404 in different situations, 200 if id is good but no data, 404 if id is not found,
 
@@ -95,6 +141,4 @@ describe('', ()=> {
 
 //while waiting for pull request to be approved, making and switching to new branch to work on also changes previous branch.
 
-//refactor controllers and models folders/files
-
-//testnotes
+// refactor to use async/await?
--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const {getTopics, getArticleById} = require('./controllers/controllers.js')
+const {getTopics} = require('./controllers/topic.controllers.js')
+const {getArticleById, patchArticleById} = require('./controllers/article.controllers.js')
 const {notAPath, customErrors, sqlErrors, internalServerError} = require('./errorhandling.js')
 
 const app = express();
+app.use(express.json())
 
 app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticleById)
+app.patch('/api/articles/:article_id', patchArticleById)
+
 
 // handle:global, custom, psql, server errors
 app.all('/api/*', notAPath)
