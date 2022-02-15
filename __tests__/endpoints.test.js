796efeba638543bc18a8d1ef6ce1f2f3f295eee9
reece-b4 <reecebalfour@gmail.com>
2022-02-15 11:43:22 +0000

    complete ticket 14, refactor error handling, acknowledge !empty array assertion in previous commit to ticket 3, add .length assertion to ticket 3

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -13,6 +13,7 @@ afterAll(() => {
   return db.end();
 });
 
+// is not empty array assertion ok here as that table shouldnt ever be empty from the start? For purpose of this task yes as we are seeding with this data?
 describe("/api/topics", () => {
   describe("GET", () => {
     test("200 - responds with array of topic objects with properties: slug, description", () => {
@@ -21,6 +22,7 @@ describe("/api/topics", () => {
         .expect(200)
         .then((res) => {
           expect(res.body.topics).not.toEqual([]);
+          expect(res.body.topics.length).toBe(3);
           res.body.topics.forEach((topic) => {
             expect.objectContaining({
               slug: expect.any(String),
@@ -30,7 +32,7 @@ describe("/api/topics", () => {
         });
     });
   });
-  test("404 - responds with message path not found", () => {
+  test("404 - given non existent path responds with message path not found <GLOBAL>", () => {
     return request(app)
       .get("/api/not-a-path")
       .expect(404)
@@ -39,4 +41,48 @@ describe("/api/topics", () => {
       });
   });
 });
-//more errors?
+
+//ticket 14
+describe("/api/articles/:article_id", () => {
+  describe("GET", () => {
+    test("200 - responds with article object with properties: author<username from users table>, title, article_id, body, topic, created_at, votes", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then((res) => {
+          expect(res.body.article).toEqual({
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: expect.any(String),
+            votes: 100,
+          });
+        });
+    });
+    test("400 - given invalid id data type, returns message: bad request", () => {
+      return request(app)
+        .get("/api/articles/not-an-id")
+        .expect(400)
+        .then((res) => {
+          expect(res.body.msg).toBe("bad request");
+        });
+    });
+    test("404 - given correct id with no data, returns message: not found", () => {
+      return request(app)
+        .get("/api/articles/999999")
+        .expect(404)
+        .then((res) => {
+          expect(res.body.msg).toBe("not found");
+        });
+    });
+  });
+});
+
+//promise.all in controller to prevent conflicts of empty array being 200 and 404 in different situations, 200 if id is good but no data, 404 if id is not found,
+
+//   sql injection
+//data mutation
+//potentially refactor created_at in get article by id path
+//when commit check change to previous code, added line to check no empty array but commited with previous push by accident without noting it, added.length assertion to get/api/topics, refactored error handling
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const {getTopics, getArticleById} = require('./controllers/controllers.js')
-const {sqlErrors, internalServiceError} = require('./errorhandling.js')
+const {notAPath, customErrors, sqlErrors, internalServerError} = require('./errorhandling.js')
 
 const app = express();
 
 app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticleById)
 
-app.all('/api/*', sqlErrors)
-app.use(internalServiceError)
+// handle:global, custom, psql, server errors
+app.all('/api/*', notAPath)
+app.use(customErrors)
+app.use(sqlErrors)
+app.use(internalServerError)
 
 module.exports = app
